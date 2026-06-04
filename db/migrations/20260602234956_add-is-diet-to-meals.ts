513f6af9ad7bee4c02e69ec41268070c7de2afa9
Luan <luanmatheus373@gmail.com>
2026-06-03 22:02:59 -0300

    fix: make is_diet column not nullable

--- db/migrations/20260602234956_add-is-diet-to-meals.ts
+++ db/migrations/20260602234956_add-is-diet-to-meals.ts
@@ -2,7 +2,7 @@ import type { Knex } from 'knex'
 
 export async function up (knex: Knex): Promise<void> {
   await knex.schema.alterTable('meals', (table) => {
-    table.boolean('is_diet')
+    table.boolean('is_diet').notNullable()
   })
 }
 
